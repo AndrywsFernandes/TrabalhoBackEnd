467d34690bcf805c54da56f7185444b3f99d631f
DiegoBololo <you@example.com>
2024-11-05 18:24:12 -0300

    Alterações

--- cliente_management/clientes/forms.py
+++ cliente_management/clientes/forms.py
@@ -7,7 +7,7 @@ from .models import Cliente
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['nome', 'email', 'telefone', 'endereco', 'data_de_nascimento', 'cpf']
+        fields = ['nome', 'email', 'telefone', 'endereco', 'data_de_nascimento', 'cpf', 'sexo']
         widgets = {
             'data_de_nascimento': forms.DateInput(attrs={'type': 'date'}, format='%d/%m/%Y'),
         }
--- cliente_management/clientes/models.py
+++ cliente_management/clientes/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
 from django.db import models
-from django.db import models
-from django.db import models
+
 class Cliente(models.Model):
+    MASCULINO = 'M'
+    FEMININO = 'F'
+    OUTRO = 'O'
+    
+    SEXO_CHOICES = [
+        (MASCULINO, 'Masculino'),
+        (FEMININO, 'Feminino'),
+        (OUTRO, 'Outro'),
+    ]
+    
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     telefone = models.CharField(max_length=15)
     endereco = models.TextField()
-    data_de_nascimento = models.DateField(null=True, blank=True) 
+    data_de_nascimento = models.DateField(null=True, blank=True)
     cpf = models.CharField(max_length=11, unique=True, null=True, blank=True)
-  
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, null=True, blank=True)
+
     def __str__(self):
         return self.nome
+
